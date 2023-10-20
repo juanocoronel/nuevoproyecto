f5e77f360c8e250cd7018625592bf7702c037727
juanocoronel <juanocoronell@gmail.com>
2023-10-19 22:50:43 -0300

    install TW

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,13 +2,19 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import "./../../../output.css"
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello !!</h1>
+			<h1 class="font-bold underline">
+    Hello world!
+  </h1>
+  <span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">Badge</span>
+  <span class="inline-flex items-center rounded-md bg-pink-50 px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">Badge</span>
+
 			<p>
 				<img src={rigoImageUrl} />
 			</p>
